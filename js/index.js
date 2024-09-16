fcb9cd713fe87ef9bb1c963305f1ceed40a39276
Ian Pichardo <ian.pichardo@hotmail.com>
2024-09-16 16:52:55 -0400

    More Projects

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-console.log("Hello")
+
+const file = document;
+const userinfo = file.getElementById('user');
+const greemtext = file.getElementById('greemtext')
+
+function ImgOver(object) {
+
+    actual = object.type
+
+    console.log(actual)
+
+    if (actual == "mouseover") {
+        greemtext.style.display = 'flex';
+        return;
+    }
+
+    greemtext.style.display = 'none';
+}
+
+userinfo.addEventListener("mouseover", ImgOver);
+userinfo.addEventListener("mouseout", ImgOver);
---
+++ ts/index.ts
@@ -0,0 +1,10 @@
+// function test(name: string|null): string {
+//     return `Hola, ${name} `;
+// }
+
+// function ImgOver(object: ImageBitmap) {
+
+// }
+
+// const userName: string = "Carlos";
+// console.log(test(userName));
